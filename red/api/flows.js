ef454a28ce9899bcf6b688bb3875b926d51a4d1d
Alex Wong <17346712+chantlong@users.noreply.github.com>
2018-03-30 17:47:25 +0900

    debug

--- red/api/flows.js
+++ red/api/flows.js
@@ -14,70 +14,104 @@
  * limitations under the License.
  **/
 
-var log;
-var redNodes;
-var settings;
+var log
+var redNodes
+var settings
 
 module.exports = {
-    init: function(runtime) {
-        settings = runtime.settings;
-        redNodes = runtime.nodes;
-        log = runtime.log;
-    },
-    get: function(req,res) {
-        var version = req.get("Node-RED-API-Version")||"v1";
-        if (version === "v1") {
-            log.audit({event: "flows.get",version:"v1"},req);
-            res.json(redNodes.getFlows().flows);
-        } else if (version === "v2") {
-            log.audit({event: "flows.get",version:"v2"},req);
-            res.json(redNodes.getFlows());
-        } else {
-            log.audit({event: "flows.get",version:version,error:"invalid_api_version"},req);
-            res.status(400).json({code:"invalid_api_version", message:"Invalid API Version requested"});
-        }
-    },
-    post: function(req,res) {
-        var version = req.get("Node-RED-API-Version")||"v1";
-        var flows = req.body;
-        var deploymentType = req.get("Node-RED-Deployment-Type")||"full";
-        log.audit({event: "flows.set",type:deploymentType,version:version},req);
-        if (deploymentType === 'reload') {
-            redNodes.loadFlows().then(function() {
-                res.status(204).end();
-            }).otherwise(function(err) {
-                log.warn(log._("api.flows.error-reload",{message:err.message}));
-                log.warn(err.stack);
-                res.status(500).json({error:"unexpected_error", message:err.message});
-            });
-        } else {
-            var flowConfig = flows;
-            if (version === "v2") {
-                flowConfig = flows.flows;
-                if (flows.hasOwnProperty('rev')) {
-                    var currentVersion = redNodes.getFlows().rev;
-                    if (currentVersion !== flows.rev) {
-                        //TODO: log warning
-                        return res.status(409).json({code:"version_mismatch"});
-                    }
-                }
-            } else if (version !== 'v1') {
-                log.audit({event: "flows.set",version:version,error:"invalid_api_version"},req);
-                res.status(400).json({code:"invalid_api_version", message:"Invalid API Version requested"});
-            }
-            redNodes.setFlows(flowConfig,deploymentType).then(function(flowId) {
-                if (version === "v1") {
-                    res.status(204).end();
-                } else if (version === "v2") {
-                    res.json({rev:flowId});
-                } else {
-                    // TODO: invalid version
-                }
-            }).otherwise(function(err) {
-                log.warn(log._("api.flows.error-save",{message:err.message}));
-                log.warn(err.stack);
-                res.status(500).json({error:"unexpected_error", message:err.message});
-            });
-        }
+  init: function(runtime) {
+    settings = runtime.settings
+    redNodes = runtime.nodes
+    log = runtime.log
+  },
+  get: function(req, res) {
+    var version = req.get('Node-RED-API-Version') || 'v1'
+    if (version === 'v1') {
+      log.audit({ event: 'flows.get', version: 'v1' }, req)
+      res.json(redNodes.getFlows().flows)
+    } else if (version === 'v2') {
+      log.audit({ event: 'flows.get', version: 'v2' }, req)
+      res.json(redNodes.getFlows())
+    } else {
+      log.audit(
+        { event: 'flows.get', version: version, error: 'invalid_api_version' },
+        req
+      )
+      res
+        .status(400)
+        .json({
+          code: 'invalid_api_version',
+          message: 'Invalid API Version requested'
+        })
     }
+  },
+  post: function(req, res) {
+    var version = req.get('Node-RED-API-Version') || 'v1'
+    var flows = req.body
+    var deploymentType = req.get('Node-RED-Deployment-Type') || 'full'
+    log.audit(
+      { event: 'flows.set', type: deploymentType, version: version },
+      req
+    )
+    if (deploymentType === 'reload') {
+      console.log('-----------in reload --------------')
+      redNodes
+        .loadFlows()
+        .then(function() {
+          res.status(204).end()
+        })
+        .otherwise(function(err) {
+          log.warn(log._('api.flows.error-reload', { message: err.message }))
+          log.warn(err.stack)
+          res
+            .status(500)
+            .json({ error: 'unexpected_error', message: err.message })
+        })
+    } else {
+      var flowConfig = flows
+      if (version === 'v2') {
+        flowConfig = flows.flows
+        if (flows.hasOwnProperty('rev')) {
+          var currentVersion = redNodes.getFlows().rev
+          if (currentVersion !== flows.rev) {
+            //TODO: log warning
+            return res.status(409).json({ code: 'version_mismatch' })
+          }
+        }
+      } else if (version !== 'v1') {
+        log.audit(
+          {
+            event: 'flows.set',
+            version: version,
+            error: 'invalid_api_version'
+          },
+          req
+        )
+        res
+          .status(400)
+          .json({
+            code: 'invalid_api_version',
+            message: 'Invalid API Version requested'
+          })
+      }
+      redNodes
+        .setFlows(flowConfig, deploymentType)
+        .then(function(flowId) {
+          if (version === 'v1') {
+            res.status(204).end()
+          } else if (version === 'v2') {
+            res.json({ rev: flowId })
+          } else {
+            // TODO: invalid version
+          }
+        })
+        .otherwise(function(err) {
+          log.warn(log._('api.flows.error-save', { message: err.message }))
+          log.warn(err.stack)
+          res
+            .status(500)
+            .json({ error: 'unexpected_error', message: err.message })
+        })
+    }
+  }
 }
