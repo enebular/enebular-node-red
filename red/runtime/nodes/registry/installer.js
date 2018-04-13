aa394d923c945ea52eb4d9497cfa200262707805
Alex Wong <17346712+chantlong@users.noreply.github.com>
2018-04-13 17:51:40 +0900

    update installer

--- red/runtime/nodes/registry/installer.js
+++ red/runtime/nodes/registry/installer.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-
+var RED = require('../../../../red/red.js')
 var when = require('when')
 var path = require('path')
 var fs = require('fs')
